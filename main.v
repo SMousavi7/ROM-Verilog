c3d87cf90e32de04c150aa3eaa9d752a811a2dc5
Seyed Sadra Mousavi <120718390+SMousavi7@users.noreply.github.com>
2024-10-10 10:20:51 -0700

    Update main.v

--- main.v
+++ main.v
@@ -10,16 +10,16 @@
 
 
 module main(
-    input[3:0] Rom_1,//vorodi rom1
-    input[1:0] Rom_2,//vorodi rom2
-    input[1:0] Rom_3,//vorodi rom3
-    input[1:0] Rom_4,//vorodi rom4
-    input[1:0] Rom_5,//vorodi rom5
-    input[2:0] Rom_6,//vorodi rom6
+    input[3:0] Rom_1,
+    input[1:0] Rom_2,
+    input[1:0] Rom_3,
+    input[1:0] Rom_4,
+    input[1:0] Rom_5,
+    input[2:0] Rom_6,
     output [31:0] SID
 );
-    wire[5:0] out_rom1;//khoroji rom1
-    wire[31:0] out_rom2, out_rom3, out_rom4, out_rom5, out_rom6;//khoroji rom haye 2 ta 6 va vorodi haye mux1 va mux2 bejoz rom6
+    wire[5:0] out_rom1;
+    wire[31:0] out_rom2, out_rom3, out_rom4, out_rom5, out_rom6;
     wire[31:0] out_mux1, out_mux2;
     wire[31:0] out_ALU;
     reg[31:0] temp_for_if_ALU;
@@ -27,18 +27,16 @@ module main(
     
 
 
-    rom_1 r1(Rom_1, out_rom1);//inja select mikone kodom adad biad biron az rom1 
-    rom_2 r2(Rom_2, out_rom2);//inja select mikone kodom adad biad biron az rom2
-    rom_3 r3(Rom_3, out_rom3);//inja select mikone kodom adad biad biron az rom3
-    rom_4 r4(Rom_4, out_rom4);//inja select mikone kodom adad biad biron az rom4
-    rom_5 r5(Rom_5, out_rom5);//inja select mikone kodom adad biad biron az rom5
-    rom_6 r6(Rom_6, out_rom6);//inja select mikone kodom adad biad biron az rom6
-    mux_1 m1(out_rom2, out_rom3, out_rom1[5], out_mux1);//inja select mikone kodom adad biad biron az mux1
-    mux_2 m2(out_rom4, out_rom5, out_rom1[4], out_mux2);//inja select mikone kodom adad biad biron az mux2
-    ALU alu(out_mux1, out_mux2, out_rom1[3:0], out_ALU, Cout);//in dige joz marahele akhare va vorodi haye asli ro pass dadim be alu
-    // always @(*) begin
-    //     $display("salam %h", out_ALU[2:0]);
-    // end
+    rom_1 r1(Rom_1, out_rom1);
+    rom_2 r2(Rom_2, out_rom2);
+    rom_3 r3(Rom_3, out_rom3);
+    rom_4 r4(Rom_4, out_rom4);
+    rom_5 r5(Rom_5, out_rom5);
+    rom_6 r6(Rom_6, out_rom6);
+    mux_1 m1(out_rom2, out_rom3, out_rom1[5], out_mux1);
+    mux_2 m2(out_rom4, out_rom5, out_rom1[4], out_mux2);
+    ALU alu(out_mux1, out_mux2, out_rom1[3:0], out_ALU, Cout);
+    
     always @(*) begin
         if(out_ALU[2:0] == 12'h000) begin
             temp_for_if_ALU = 32'hzzzzzzzz;
@@ -57,9 +55,5 @@ module main(
         end
     end
 
-    // always @(*) begin
-    //     $display("alu : %h", out_ALU);
-    //     $display("rom6 : %h", out_rom6);
-    // end
     assign SID = out_ALU + out_rom6;  
 endmodule
